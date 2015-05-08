0dbaa97d08b960168c45a2d8ca4c2e39ee33324f
aidepon <yamoa311@newschool.edu>
2015-05-08 00:38:56 -0400

    update

--- room.js
+++ room.js
@@ -1,3 +1,5 @@
+// LIGHT FUNCTION
+
 $(function(){  
     var lightsOn = true;
 
@@ -12,3 +14,76 @@ $(function(){
         
     }); 
 });
+
+//  ROOM 1 FUNCTION
+// W : window > X, mirror > X, flower > X
+// B : window > X, mirror = event, door > home (after event > r2
+
+var mirrorStat = false;
+var doorStat = false;
+
+mirror.onload = function () {
+	var mirrorClicks = document.getElementById('mirror'); 
+	mirrorClicks.addEventListener('click', mirrorClicks);
+	
+door.onload = function () {
+	var doorClicks = document.getElementById('door'); 
+	doorClicks.addEventListener('click', doorClicks);
+	
+function mirrorClick () {
+	if (mirrorStat == false) {
+		document.getElementById('mirror').setAttribute('id', 'mirrorClick');
+		
+	if (doorStat == true) {
+			document.getElementById('doorClick').setAttribute('id', 'door');}
+
+		mirrorStat = true;
+		doorStat = false;
+		
+		results ()
+	}}
+	
+function doorClick () {
+	if (doorStat == false) {
+		document.getElementById('door').setAttribute('id', 'doorClick');
+		
+	if (mirrorStat == true) {
+			document.getElementById('mirrorClick').setAttribute('id', 'mirror');}
+						
+		doorStat = true;
+		mirrorStat = false;
+		
+		widow.location.href= "":
+		
+	}}
+	
+	
+
+//  ROOM 2 FUNCTION
+// W : mother > X , twirl > X, door > r1 (after event > r3
+// B : mother = event , kill mother > game over , door > r1
+
+
+
+//  ROOM 3 FUNCTION
+// W : eat cake > X , draw > X, diary = event
+// B : door > r2 (after event r4) , write > X (click 30x makes words appear on wall), sleep > X
+
+
+
+//  ROOM 4 FUNCTION
+// W : pluck petals > fade out to r3 , pick knife = knife is picked, door > r3 (after events (2) r5)
+// B : touch hands > game over , stab hand = event, stab self > game over
+
+
+
+//  ROOM 5 FUNCTION
+// W : open window > game over , painting > X, lay in bed > X
+// B : write letter = event (letter appears on table) , painting > X (person dies), leave > game over (after event, end1)
+
+
+
+//  ROOM 6 FUNCTION
+// W : lay triangle > X (triangle is placed)
+// B : goodbye = end2 (sinks into water)
+
